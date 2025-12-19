f06d11f6a743dd63629a8c85f20f18cdda927e41
TwinCoyote <twincoyote0732@gmail.com>
2025-12-18 21:22:23 -0600

    Refactor serializers and views to include image and video handling; remove README and requirements files.

--- backend/portfolio/serializers.py
+++ backend/portfolio/serializers.py
@@ -1,7 +1,27 @@
+# pylint: disable=C0115
+
+
 from rest_framework import serializers
-from .models import Proyectos
+from .models import Proyectos, ImagenProyecto, VideoProyecto
+
+
+class ImagenProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImagenProyecto
+        fields = ["proyecto", "imagen", "descripcion", "fecha_creacion"]
+
+
+class VideoProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VideoProyecto
+        fields = ["proyecto", "video", "descripcion", "fecha_creacion"]
+
 
 class ProyectosSerializer(serializers.ModelSerializer):
+    imagenes = ImagenProyectoSerializer(many=True, read_only=True)
+    videos_archivo = VideoProyectoSerializer(many=True, read_only=True)
+
     class Meta:
         model = Proyectos
-        fields=["id","titulo","descripcion","link","imagen","videos","fecha_creacion"]
+        fields = ["id", "titulo", "descripcion", "link",
+                  "imagen", "tecnologias", "videos", "fecha_creacion", "imagenes", "videos_archivo"]
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Proyectos
+from .serializers import ProyectosSerializer
 
 class ProyectosListCreate(generics.ListCreateAPIView):
+    queryset = Proyectos.objects.all()
+    serializer_class = ProyectosSerializer
+
+class ProyectosRetriveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Proyectos.objects.all()
     serializer_class = ProyectosSerializer
