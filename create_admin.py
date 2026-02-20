ddbe729665d86ee1991a871e82d381e94d299d99
TwinCoyote <twincoyote0732@gmail.com>
2026-02-20 16:00:25 -0600

    Fix 11

--- create_admin.py
+++ create_admin.py
@@ -2,16 +2,19 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+# Ajusta esto según tu proyecto
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
 User = get_user_model()
 
 username = 'admin'
+email = 'admin@example.com'
 password = 'Nievesilla2001B%.'
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, password=password)
+    User.objects.create_superuser(
+        username=username, email=email, password=password)
     print(f"Superuser '{username}' creado exitosamente.")
 else:
     print(f"El usuario '{username}' ya existe.")
